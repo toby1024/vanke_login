eedcff71c87cf2ab6e743175862a7762c255104a
张斌 <zhangbin@skio.cn>
2017-11-29 16:57:16 +0800

    fix bug

--- login.rb
+++ login.rb
@@ -4,15 +4,30 @@ class Login
   require 'json'
   require "base64"
 
-  phone = Base64.encode64(ARGV[0])
-  password = Base64.encode64(ARGV[1])
+  phone = Base64.strict_encode64(ARGV[0])
+  password = Base64.strict_encode64(ARGV[1])
+
+  p phone
+  p password
     
   uri = URI.parse("https://union.vanke.com/api/Member/Login")
   
   Net::HTTP.start(uri.host, uri.port, :use_ssl => uri.scheme == 'https') do |http|
     request = Net::HTTP::Post.new(uri,'Content-Type' => 'application/json')
-    request.body = {body:{AccountName: phone,AgentId:"",AuthCode:"",LoginType:1,Password: password,ThirdPartyUserId:"",Token:""},header:{version:"",clientOSType:"android",accessToken:"",refreshToken:"",sign:""}}.to_json
-    request.set_form_data(AccountName: 'MTM2MzQxNzE0MDQ=', Password: 'aGFuaGFuMTIzNA==', LoginType: 1, AgentId:'',AuthCode:'',Token:'')
+    request.body = { body: { AccountName: phone, 
+                             AgentId:"", 
+                             AuthCode:"",
+                             LoginType:1, 
+                             Password: password, 
+                             ThirdPartyUserId:"",Token:""
+                            },
+                     header:{ version:"",
+                              clientOSType:"android",
+                              accessToken:"",
+                              refreshToken:"",
+                              sign:""
+                            }
+                    }.to_json
     response = http.request request # Net::HTTPResponse object
     p response.body
   end
