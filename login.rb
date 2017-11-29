83b5069d5cbf9dd497502ad587d3318f09c8f367
张斌 <zhangbin@skio.cn>
2017-11-29 17:09:39 +0800

    每隔10小时登录一次

--- login.rb
+++ login.rb
@@ -8,24 +8,26 @@ class Login
   password = Base64.strict_encode64(ARGV[1])
     
   uri = URI.parse("https://union.vanke.com/api/Member/Login")
-  
-  Net::HTTP.start(uri.host, uri.port, :use_ssl => uri.scheme == 'https') do |http|
-    request = Net::HTTP::Post.new(uri,'Content-Type' => 'application/json')
-    request.body = { body: { AccountName: phone, 
-                             AgentId:"", 
-                             AuthCode:"",
-                             LoginType:1, 
-                             Password: password, 
-                             ThirdPartyUserId:"",Token:""
-                            },
-                     header:{ version:"",
-                              clientOSType:"android",
-                              accessToken:"",
-                              refreshToken:"",
-                              sign:""
-                            }
-                    }.to_json
-    response = http.request request # Net::HTTPResponse object
-    p response.body
+  while(true)
+    Net::HTTP.start(uri.host, uri.port, :use_ssl => uri.scheme == 'https') do |http|
+      request = Net::HTTP::Post.new(uri,'Content-Type' => 'application/json')
+      request.body = { body: { AccountName: phone, 
+                              AgentId:"", 
+                              AuthCode:"",
+                              LoginType:1, 
+                              Password: password, 
+                              ThirdPartyUserId:"",Token:""
+                              },
+                      header:{ version:"",
+                                clientOSType:"android",
+                                accessToken:"",
+                                refreshToken:"",
+                                sign:""
+                              }
+                      }.to_json
+      response = http.request request # Net::HTTPResponse object
+      p response.body
+    end
+    sleep(10*60*60)
   end
 end
